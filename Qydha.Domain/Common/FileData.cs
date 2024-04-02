c691031d3c489e6f82cebfcd171146308a4a2510
assem morsy <assemmorsy@outlook.com>
2024-04-02 20:41:47 +0200

    change serialzation for notification payload

--- Qydha.Domain/Common/FileData.cs
+++ Qydha.Domain/Common/FileData.cs
@@ -1,8 +1,10 @@
-﻿namespace Qydha.Domain.Common;
+﻿using Newtonsoft.Json;
+
+namespace Qydha.Domain.Common;
 
 public class FileData
 {
+    [JsonProperty(PropertyName = "Url")]
     public string Url { get; set; } = string.Empty;
     public string Path { get; set; } = string.Empty;
-
 }
