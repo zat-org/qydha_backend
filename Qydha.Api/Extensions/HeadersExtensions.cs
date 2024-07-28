5836e79d408875f461e9007fde8271a60d2176ee
Assem Morsy <assemmorsy@outlook.com>
2024-07-28 15:55:23 +0300

    fix x-info logging and serialization

--- Qydha.Api/Extensions/HeadersExtensions.cs
+++ Qydha.Api/Extensions/HeadersExtensions.cs
@@ -5,9 +5,6 @@ namespace Qydha.API.Extensions;
 
 public static class HeadersExtensions
 {
-    // " => X-INFO : "{environment: production, appVersion: 2.5.3, platform: Android, deviceName: Infinix X6823C, deviceId: 7b5f44f0-4c3a-11ef-8d98-818bdf77a1a7}" " 
-
-
     public static XInfoData GetXInfoHeaderData(this IHeaderDictionary headers)
     {
         if (headers.TryGetValue("X-INFO", out StringValues xInfoAsStringArr))
@@ -16,13 +13,8 @@ public static class HeadersExtensions
             {
                 string xInfoAsString = xInfoAsStringArr.FirstOrDefault() ?? "";
                 if (string.IsNullOrEmpty(xInfoAsString)) return new XInfoData();
-                xInfoAsString = xInfoAsString.Replace("{", "{\"")
-                      .Replace("}", "\"}")
-                      .Replace(": ", "\": \"")
-                      .Replace(", ", "\", \"");
                 XInfoData data = JsonConvert.DeserializeObject<XInfoData>(xInfoAsString) ?? new XInfoData();
                 return data;
-
             }
             catch (Exception)
             {
--- Qydha.Api/Program.cs
+++ Qydha.Api/Program.cs
@@ -41,10 +41,9 @@ app.UseAuthorization();
 // app.UseStaticFiles();
 app.UseSerilogRequestLogging(op =>
 {
-    op.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms {NewLine} => UserId : {UserId} {NewLine} => Token Type : {TokenType} {NewLine} => Client IP : {ClientIp} {NewLine} => X-INFO : {xINFO} {NewLine} => TraceId : {RequestId} ";
+    op.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms {NewLine} => UserId : {UserId} {NewLine} => Token Type : {TokenType} {NewLine} => Client IP : {ClientIp} {NewLine} => X-INFO : Environment : {Environment} Platform : {Platform} DeviceName : {DeviceName} AppVersion : {AppVersion} DeviceId : {DeviceId} {NewLine} => TraceId : {RequestId} ";
     op.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
     {
-        diagnosticContext.Set("xINFO", httpContext.Request.Headers["x-info"].ToString());
         diagnosticContext.Set("ClientIp", httpContext.Request.Headers["X-Real-IP"].ToString());
         diagnosticContext.Set("UserId", httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "user Id not provided");
         diagnosticContext.Set("TokenType", httpContext.User.FindFirst(ClaimsNamesConstants.TokenType)?.Value ?? "Token type not provided");
